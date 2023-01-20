0729d4e9c5f077091aa2cb3c32a8ba54b7f5721c
Akash Kumar Gupta <117676075+Akashgupta19971998@users.noreply.github.com>
2023-01-20 12:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,9 @@
 
 function palindrome(str){
  const len = str.length;  
-  
-    // Use for loop to divide the words into 2 half  
+    
     for (let i = 0; i < len / 2; i++) {  
-  
-        // validate the first and last characters are same  
+   
         if (str[i] !== str[len - 1 - i]) {  
             console.log( 'false');  
         }  
