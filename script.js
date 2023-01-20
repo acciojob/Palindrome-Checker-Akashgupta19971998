bbd6adaea1398d5c66e8393eb6d794174b7c8063
Akash Kumar Gupta <117676075+Akashgupta19971998@users.noreply.github.com>
2023-01-20 12:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function palindrome(str){
     for (let i = 0; i < len / 2; i++) {  
    
         if (str[i] !== str[len - 1 - i]) {  
-            console.log( 'false');  
+            return false;
         }  
     }  
-    console.log( 'true');
+    return true;
 }
 module.exports = palindrome
